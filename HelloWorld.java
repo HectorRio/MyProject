9f4efcdd0d0a016b787ef78938ed332334d0366c
HectorRio <44659203+HectorRio@users.noreply.github.com>
2019-03-12 09:01:57 +0800

    Debug
    Resolved some bugs

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
-  public static void main(string[] args){
+  public static void main(String[] args){
     
-    systemout.println("Hello World!");
+    System.out.println("Hello World!");
   
   }
 }
